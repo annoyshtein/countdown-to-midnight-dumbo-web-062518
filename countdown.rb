7aaa6e236c7e7450a4327d55752cdc06c00d66d5
Alex Neustein <alex.neustein@gmail.com>
2018-06-19 23:17:18 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
-def countdown
-  counter = 12
+def countdown(seconds)
+  counter = seconds
 while counter > 0
-  puts "The current number is less than 20."
+  puts "#{number} SECOND(S)!"
   counter += 1
 end
 end
